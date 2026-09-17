2d0e0c30d5791c19462f9cf17aed108bb9c1d644
Vaibhav Barman <vaibhavbarman2609@gmail.com>
2026-09-17 22:48:29 +0530

    feat: add persistent Chrome profile

--- src/browser.py
+++ src/browser.py
@@ -1,5 +1,13 @@
 from selenium import webdriver
+from pathlib import Path
+
 
 def setup():
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+
+    profile_path = Path(__file__).parent.parent / "browser_data"
+    options.add_argument(f"--user-data-dir={profile_path}")
+
+    driver = webdriver.Chrome(options=options)
+
     return driver
